ddc4a23ca0e4123f5540ab61eb01e84e8d23bbae
RachelBailin <rachelebailin@gmail.com>
2014-01-31 17:23:34 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KarmaApp::Application.config.secret_key_base = '41c9bba23a18d2d26d8be2f47afb3d93ba0339ad3897f96f0f0ac7d30967369e900e6437bdd1b11fffab5df153597e7f612a062f944ab0831c13fa27b8e67be2'
